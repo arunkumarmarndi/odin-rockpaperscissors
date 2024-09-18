6b44cb3b6226d165e00e9ba4f0f13150b5e673b8
arunkumarmarndi <arunkumarmarndi1994@gmail.com>
2024-09-18 20:12:22 +0530

    Step 1: js function for returning a computerChoice from rock,paper,scissors

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Step 1: Function to randomly return "rock", "paper", or "scissors"
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * 3);
+    return choices[randomIndex];
+}
